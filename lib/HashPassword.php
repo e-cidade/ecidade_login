792834dabbb876d6498488b56556dafcbb1efad1
Vitor Mattos <vitor@php.rio>
2024-06-13 11:53:21 -0300

    Update HashPassword.php

--- lib/HashPassword.php
+++ lib/HashPassword.php
@@ -2,15 +2,17 @@
 
 namespace OCA\EcidadeLogin;
 
+use OCA\UserBackendSqlRaw\IHashPassword;
+
 class HashPassword implements IHashPassword {
 	public function validate(string $password, string $hash): bool {
-		if (md5(md5($password)) === $hash) {
+		if (sha1(md5($password)) === $hash) {
 			return true;
 		};
 		return $password === $hash;
 	}
 
 	public function hashPassword(string $password): string {
-		return md5($password);
+		return sha1(md5($password));
 	}
 }
